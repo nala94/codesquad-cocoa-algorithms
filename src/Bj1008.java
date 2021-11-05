7de5cc8263ae026c77f9abcb5466b2baf9b88bfa
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-05 18:07:47 +0900

    week1

---
+++ src/Bj1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bj1008{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = Double.parseDouble(sc.next());
+        double b = Double.parseDouble(sc.next());
+
+        System.out.println(a / b);
+
+    }
+}
